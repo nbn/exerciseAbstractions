287f6a7dd4f552b98b6ec2204988eafced6ea12a
Niels Bech Nielsen <nbni@kea.dk>
2023-09-27 14:50:56 +0200

    Inital setup

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Sinatra sinatra = new Sinatra();
+        Console.WriteLine($"The singer is {sinatra}"); // 1) Use getName
+        
+        // 2) Uncomment this
+        // sinatra.Sing();
+        
+        ISinger s = null; // 3) Assign New Sinatra instead of null
+        Console.WriteLine($"The singer is {s.GetName()}");
+        s.Sing();
+
+    }
+
+    interface ISinger
+    {
+        String GetName();
+        void Sing();
+
+    }
+
+    class Sinatra
+    {
+        
+    }
+}
